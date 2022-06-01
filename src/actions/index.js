f29e5ee250d5121cb5dc1fad0da99a1d7e49ba3e
Aleks G <57754379+SneakyPotts@users.noreply.github.com>
2022-06-01 17:13:46 +0300

    HeroesAddForm task ready;

---
+++ src/actions/index.js
@@ -0,0 +1,43 @@
+export const heroesFetching = () => {
+  return {
+    type: 'HEROES_FETCHING'
+  }
+}
+
+export const heroesFetched = (heroes) => {
+  return {
+    type: 'HEROES_FETCHED',
+    payload: heroes
+  }
+}
+
+export const heroesFetchingError = () => {
+  return {
+    type: 'HEROES_FETCHING_ERROR'
+  }
+}
+export const filtersFetching = () => {
+  return {
+    type: 'FILTERS_FETCHING'
+  }
+}
+
+export const filtersFetched = (heroes) => {
+  return {
+    type: 'FILTERS_FETCHED',
+    payload: heroes
+  }
+}
+
+export const filtersFetchingError = () => {
+  return {
+    type: 'FILTERS_FETCHING_ERROR'
+  }
+}
+
+export const heroCreate = (hero) => {
+  return {
+    type: 'HERO_CREATE',
+    payload: hero
+  }
+}
---
+++ src/reducers/index.js
@@ -0,0 +1,52 @@
+const initialState = {
+  heroes: [],
+  heroesLoadingStatus: 'idle',
+  filters: [],
+  filtersLoadingStatus: 'idle'
+}
+
+const reducer = (state = initialState, {type, payload}) => {
+  switch (type) {
+    case 'HEROES_FETCHING':
+      return {
+        ...state,
+        heroesLoadingStatus: 'loading'
+      }
+    case 'HEROES_FETCHED':
+      return {
+        ...state,
+        heroes: payload,
+        heroesLoadingStatus: 'idle'
+      }
+    case 'HEROES_FETCHING_ERROR':
+      return {
+        ...state,
+        heroesLoadingStatus: 'error'
+      }
+    case 'FILTERS_FETCHING':
+      return {
+        ...state,
+        filtersLoadingStatus: 'loading'
+      }
+    case 'FILTERS_FETCHED':
+      return {
+        ...state,
+        filters: payload,
+        filtersLoadingStatus: 'idle'
+      }
+    case 'FILTERS_FETCHING_ERROR':
+      return {
+        ...state,
+        filtersLoadingStatus: 'error'
+      }
+    case 'HERO_CREATE':
+      return {
+        ...state,
+        heroes: [...state.heroes, payload]
+      }
+    default:
+      return state
+  }
+}
+
+export default reducer;
