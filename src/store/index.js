3b11e7d1e4b77af552afd33f6eb52599297a1397
Aleks G <57754379+SneakyPotts@users.noreply.github.com>
2022-06-01 22:05:12 +0300

    create new actions with combine several aold actions; added Redux toolkit; replaced store to configureStore;

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,48 @@
-import { createStore } from 'redux';
-import {combineReducers} from "@reduxjs/toolkit";
 import heroesReducer from "../reducers/heroes";
 import filterReducer from "../reducers/filters";
+import {configureStore} from "@reduxjs/toolkit";
 
-const reducer = combineReducers({
-  heroesReducer,
-  filterReducer
-});
+// const reducer = combineReducers({
+//   heroesReducer,
+//   filterReducer
+// });
 
-const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+const stringMiddleware = (store /*{dispatch, getState}*/) => (next /*dispatch*/) => (action) => {
+  if (typeof action === 'string') {
+    return next({
+      type: action
+    });
+  }
+  return next(action);
+}
+
+// const enhancer = createStore => (...args) => {
+//   const store = createStore(...args);
+//
+//   const oldDispatch = store.dispatch;
+//   store.dispatch = (action) => {
+//     if (typeof action === 'string') {
+//       return oldDispatch({
+//         type: action
+//       });
+//     }
+//     return oldDispatch(action);
+//   }
+//
+//   return store;
+// }
+
+// const store = createStore(
+//   reducer,
+//   compose(
+//     applyMiddleware(ReduxThunk,stringMiddleware),
+//     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+//   ));
+
+const store = configureStore({
+  reducer: {heroesReducer, filterReducer},
+  middleware: getDefaultMiddleware => getDefaultMiddleware().concat(stringMiddleware), //ReduxThunk включён уже
+  devTools: process.env.NODE_ENV !== 'production', //продакшн версия или разработка
+})
 
 export default store;
